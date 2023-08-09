e9ec684db80735602970ccd10af3c108f6f99b46
Uwe Krohn <krohn@fh-luebeck.de>
2023-08-09 11:10:00 +0200

    ...

--- src/test/java/daytime/actor/DaytimeActorTest.java
+++ src/test/java/daytime/actor/DaytimeActorTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public  class EchoActorTest {
+public  class DaytimeActorTest {
 
 	@ParameterizedTest
 	@CsvSource({"'Hallo'",
